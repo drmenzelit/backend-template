13dfa6f178829c85e7b764e54546d3665851f5ed
Hannes Papenberg <info@joomlager.de>
2019-03-29 16:41:25 +0100

    Further work on table column filters

--- layouts/joomla/table/columnfilter.php
+++ layouts/joomla/table/columnfilter.php
@@ -13,25 +13,42 @@ defined('JPATH_BASE') or die;
 $columns = $displayData['columns'];
 $id = $displayData['id'];
 $name = $displayData['name'];
+$user = JFactory::getUser();
+$document = JFactory::getDocument();
+$selected = $user->getParam('joomla.table.' . $name);
+if (is_null($selected))
+{
+	$selected = array_column($columns, 'name');
+}
 
+$css = [];
+foreach ($columns as $i => $column)
+{
+	if (!in_array($column['name'], $selected))
+	{
+		$css[] = '#' . $id . ' tr > *:nth-child(' . ($i + 1) . ')';
+	}
+}
+
+$document->addStyleDeclaration(implode(',', $css) . '{width:0; display: none !important;}');
 ?>
 <div class="dropdown pull-right">
 	<a class="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 		<span class="fa fa-filter" title="Filter"></span>
 	</a>
 	<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-		<form class="px-4 py-3">
+
 			<div class="form-group">
 				<?php foreach ($columns as $column) : ?>
 				<?php if (!isset($column['title'])) continue; ?>
 				<div class="form-check">
-					<input type="checkbox" class="form-check-input" id="dropdownCheck" checked <?php echo isset($column['protected']) ? 'disabled' : ''; ?>>
+					<input type="checkbox" class="form-check-input" id="dropdownCheck" <?php echo in_array($column['name'], $selected) ? 'checked ' : '';?><?php echo isset($column['protected']) ? 'disabled' : ''; ?>>
 					<label class="form-check-label" for="dropdownCheck">
 						<?php echo $column['title']; ?>
 					</label>
 				</div>
 				<?php endforeach; ?>
 			</div>
-		</form>
+
 	</div>
 </div>
